3e1f537c0f5cba7f72359e171ed291262b5ea76a
silvia <silvia.alonso.pozas@gmail.com>
2020-03-10 10:33:43 +0100

    final commit

--- main.js
+++ main.js
@@ -5,20 +5,27 @@ function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
+//ejecutamos la función
 const random = getRandomNumber(100);
 console.log(random);
 
-// const num = document.querySelector('.value');
-// num.addEventListener('click',function(){})
+//estamos definiendo num como el contenido del input con id=numero
+const num = document.querySelector('#numero');
 
-// if (num > random) {
-//   console.log('Demasiado alto');
-// } else {
-//   if (num < random) {
-// console.log('Demasiado bajo');
-//   } else {
-// if (num === random) {
-//   console.log('Has ganado campeona!!!');
-// }
-//   }
-// }
+num.addEventListener('click', function() {
+  if (num > random) {
+    console.log('Demasiado alto');
+  } else {
+    if (num < random) {
+      console.log('Demasiado bajo');
+    } else {
+      if (num === random) {
+        console.log('Has ganado campeona!!!');
+      } else {
+        if (1 > num < 100) {
+          console.log('El número debe estar entre 1 y 100');
+        }
+      }
+    }
+  }
+});
