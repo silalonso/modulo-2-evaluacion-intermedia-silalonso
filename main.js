8942ea48a4f6a03ca8fe68ee4dcab966edeac165
silvia <silvia.alonso.pozas@gmail.com>
2020-03-25 14:58:18 +0100

    fix conditionals,adds ev.preventDefault,fix subject from listener

--- main.js
+++ main.js
@@ -9,23 +9,27 @@ function getRandomNumber(max) {
 const random = getRandomNumber(100);
 console.log(random);
 
-//estamos definiendo num como el contenido del input con id=numero
-const num = document.querySelector('#numero');
+//estamos definiendo num como el contenido del input con class=numero
+const num = document.querySelector('.numero');
+console.log(num);
 
-num.addEventListener('click', function() {
+// necesito ponerle el listener al botón, no al input
+
+const btn = document.querySelector('.js-submitbutton');
+console.log(btn);
+// ahora el botón es btn, que es el sujeto del listener
+
+function handleBtn(ev) {
+  ev.preventDefault();
   if (num > random) {
     console.log('Demasiado alto');
-  } else {
-    if (num < random) {
-      console.log('Demasiado bajo');
-    } else {
-      if (num === random) {
-        console.log('Has ganado campeona!!!');
-      } else {
-        if (1 > num < 100) {
-          console.log('El número debe estar entre 1 y 100');
-        }
-      }
-    }
+  } else if (num < random) {
+    console.log('Demasiado bajo');
+  } else if (num === random) {
+    console.log('Has ganado campeona!!!');
+  } else if (1 > num < 100) {
+    console.log('El número debe estar entre 1 y 100');
   }
-});
+}
+
+btn.addEventListener('click', handleBtn);
